6e5141a90528361b053804718b38e04fbb29df54
Chloe <chloe.wong@madetech.com>
2020-11-01 17:21:48 +0000

    working with integers and main function separated out

--- Program.cs
+++ Program.cs
@@ -4,10 +4,31 @@ namespace csharpTutorial
 {
     class Program
     {
+        static void WorkingWithIntegers()
+        {
+            int a = 19;
+            int b = 6;
+
+            // addition
+            int c = a + b;
+            Console.WriteLine(c);
+
+            // subtraction
+            c = a - b;
+            Console.WriteLine(c);
+
+            // multiplication
+            c = a * b;
+            Console.WriteLine(c);
+
+            // division
+            c = a / b;
+            Console.WriteLine(c);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine($"The current time is {DateTime.Now}.");
+            //WorkingWithIntegers();
         }
     }
 }
